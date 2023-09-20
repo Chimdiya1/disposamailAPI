518a01f167bfee5f224dea289378f00e2adbf571
Chimdia Anyiam <fortunechimdiya@gmail.com>
2023-09-20 21:52:56 +0100

    added ssl

--- src/db.js
+++ src/db.js
@@ -13,6 +13,7 @@ const productionConfig = {
   database: process.env.DB_DATABASE,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
+  ssl:true
 };
 let config = process.env.NODE_ENV === "production" ? productionConfig : devConfig;
 
